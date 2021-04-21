b058d3a4e8893531fadb0f4e75a16d589c5ed11e
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-04-21 19:23:19 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #author https://github.com/developeranaz (don't delete this)
-jupyter notebook --allow-root --no-browser --port $PORT
+jupyter notebook --allow-root --no-browser --port 8080
