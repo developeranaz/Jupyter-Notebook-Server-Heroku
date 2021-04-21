06e2690abc926328377223518a182904dbf1b622
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-04-21 18:00:22 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 #author https://github.com/developeranaz (don't delete this)
-#refreshing Rclone
-rclone version
-rclone copy /donate.developeranaz.txt $CLOUDNAME:
-#serving to port 8080
-rclone serve http $CLOUDNAME: --addr :$PORT
+jupyter notebook --allow-root --no-browser
